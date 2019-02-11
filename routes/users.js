4d80b3795f4bd87e385a3a6538d5a3d84bd1073b
Achilleas Kyritsakas <akiritsakas@gmail.com>
2019-02-11 18:35:45 +0200

    feat(routes.js): add PUT users/current

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,35 @@ router.get('/users/current', authJWT.required, async (ctx, next) => {
   ctx.body = { user: user.toAuthJSON() };
 });
 
+router.put('/users/current', authJWT.required, async (ctx, next) => {
+  const { user: newUser } = ctx.request.body;
+  const user = await User.findById(ctx.state.user.id);
+
+  // TODO: add validator library
+  // user email and password can change only with
+  // the users that have beeen registered through local
+  // Also password and email can not be empty strings
+  if (typeof newUser.username !== 'undefined') {
+    user.username = newUser.username;
+  }
+  if (typeof newUser.email !== 'undefined') {
+    user[user.method].email = newUser.email;
+  }
+  if (typeof newUser.bio !== 'undefined') {
+    user.bio = newUser.bio;
+  }
+  if (typeof newUser.image !== 'undefined') {
+    user.image = newUser.image;
+  }
+  if (typeof newUser.password !== 'undefined') {
+    user[user.method].hash = newUser.password;
+  }
+
+  await user.save();
+
+  ctx.body = { user: user.toAuthJSON() };
+});
+
 router.post('/users/login', authenticate('local'));
 
 router.post('/users', async (ctx, next) => {
