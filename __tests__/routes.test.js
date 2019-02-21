486812820de2cd3501dacadac120597057273740
Achilleas Kyritsakas <akiritsakas@gmail.com>
2019-02-21 17:01:59 +0200

    test(routes): Add tests for testing api auth

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,6 +1,19 @@
 const request = require('supertest');
 const server = require('../index.js');
 
+const getRandomId = () => {
+  return Math.random()
+    .toString(36)
+    .substring(2);
+};
+const data = {
+  user: {
+    email: `${getRandomId()}@email.com`,
+    password: 'password',
+    username: 'jane'
+  }
+};
+
 beforeAll(async () => {
   console.log('Jest starting!');
 });
@@ -11,10 +24,80 @@ afterAll(() => {
   console.log('server closed!');
 });
 
-describe('basic route tests', () => {
-  test('get home route GET api/users/current', async () => {
-    const response = await request(server).get('/api/users/current');
-    expect(response.status).toEqual(200);
-    expect(response.text).toContain('');
+describe('routes: users', () => {
+  let res;
+  let user;
+  let token;
+
+  test('POST api/users', async () => {
+    res = await request(server)
+      .post('/api/users')
+      .send(data);
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(Object.keys(res.body.user)).toEqual(
+      expect.arrayContaining(['username', 'email', 'token', 'bio', 'image'])
+    );
+
+    user = {
+      email: res.body.user.email,
+      password: 'password'
+    };
+  });
+
+  test('POST api/users/login', async () => {
+    res = await request(server)
+      .post('/api/users/login')
+      .send({ user });
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(Object.keys(res.body.user)).toEqual(
+      expect.arrayContaining(['username', 'email', 'token', 'bio', 'image'])
+    );
+
+    token = res.body.user.token;
+  });
+
+  test('GET api/users/current', async () => {
+    res = await request(server)
+      .get('/api/users/current')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(Object.keys(res.body.user)).toEqual(
+      expect.arrayContaining(['username', 'email', 'token', 'bio', 'image'])
+    );
+  });
+
+  test('PUT api/users/current', async () => {
+    const userUpdate = {
+      email: `${getRandomId()}@email.com`,
+      username: getRandomId()
+    };
+    res = await request(server)
+      .put('/api/users/current')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ user: userUpdate });
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(Object.keys(res.body.user)).toEqual(
+      expect.arrayContaining(['username', 'email', 'token', 'bio', 'image'])
+    );
+  });
+});
+
+describe('routes: articles', () => {
+  test('GET api/articles', async () => {
+    const res = await request(server).get('/api/articles');
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(Object.keys(res.body)).toEqual(
+      expect.arrayContaining(['articles', 'articlesCount'])
+    );
   });
 });
